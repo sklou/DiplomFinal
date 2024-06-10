b4f9f5f10d8fac887047c69021c3c302896f4de4
sklou <49943420+sklou@users.noreply.github.com>
2024-06-10 16:54:59 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '3t0tM1rPr0gn1l',
+  password: '************',
   database: 'diplomdb'
 });
 
